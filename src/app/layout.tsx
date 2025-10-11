c8a3901f199c4ca6d246243c12ec09117740e1df
tee-boy <tochukwuemmanuel690@gmail.com>
2025-10-11 10:09:56 +0100

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "../app/components/nav";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: ["300", "400", "500", "600", "700"], // choose what you need
+  variable: "--font-montserrat",
+})
 
 export const metadata: Metadata = {
   title: "Jerry Portfolio",
@@ -31,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-gray-900`}
+        className={`${montserrat.variable} font-montserrat antialiased bg-white text-gray-900`}
       >
         <Navbar />
         {children}
